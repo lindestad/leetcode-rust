255a3a5d9d4e2b5dcffe790415db6e9ecefc73fd
Daniel Lindestad <lindestad@proton.me>
2025-03-07 23:19:51 +0100

    1768 Merge strings alternately

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -2,6 +2,7 @@
 #![allow(dead_code)] // Suppress dead code warnings
 
 pub mod q1;
+pub mod q1768;
 pub mod q1790;
 pub mod q20;
 pub mod q22;
---
+++ src/problems/q1768.rs
@@ -0,0 +1,75 @@
+struct ZipMixedLength<A, B> {
+    iter1: A,
+    iter2: B,
+}
+impl<A, B> ZipMixedLength<A, B> {
+    fn new(iter1: A, iter2: B) -> Self {
+        Self { iter1, iter2 }
+    }
+}
+
+impl<A, B, C, D> Iterator for ZipMixedLength<A, B>
+where
+    A: Iterator<Item = C>,
+    B: Iterator<Item = D>,
+    C: Default,
+    D: Default,
+{
+    type Item = (C, D);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match (self.iter1.next(), self.iter2.next()) {
+            (Some(a), Some(b)) => Some((a, b)),
+            (Some(a), None) => Some((a, Default::default())),
+            (None, Some(b)) => Some((Default::default(), b)),
+            (None, None) => None,
+        }
+    }
+}
+
+// 1768. Merge Strings Alternately
+pub fn merge_alternately(word1: String, word2: String) -> String {
+    let mut result = String::new();
+
+    let iter1 = word1.chars();
+    let iter2 = word2.chars();
+
+    for (a, b) in ZipMixedLength::new(iter1, iter2) {
+        if a != '\0' {
+            result.push(a);
+        }
+        if b != '\0' {
+            result.push(b);
+        }
+    }
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_merge_alternately() {
+        let word1 = "abc".to_string();
+        let word2 = "pqr".to_string();
+        let output = merge_alternately(word1, word2);
+        assert_eq!(output, "apbqcr".to_string());
+    }
+
+    #[test]
+    fn test_merge_alternately2() {
+        let word1 = "ab".to_string();
+        let word2 = "pqrs".to_string();
+        let output = merge_alternately(word1, word2);
+        assert_eq!(output, "apbqrs".to_string());
+    }
+
+    #[test]
+    fn test_merge_alternately3() {
+        let word1 = "abcd".to_string();
+        let word2 = "pq".to_string();
+        let output = merge_alternately(word1, word2);
+        assert_eq!(output, "apbqcd".to_string());
+    }
+}
