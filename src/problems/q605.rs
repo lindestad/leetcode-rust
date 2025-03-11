8d6a7b9e8b943639aa25ff9bb34db5303e324b66
Daniel Lindestad <lindestad@proton.me>
2025-03-11 21:08:39 +0100

    init

---
+++ src/problems/q605.rs
@@ -0,0 +1,13 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn can_place_flowers(flowerbed: Vec<i32>, n: i32) -> bool {}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_() {}
+}
